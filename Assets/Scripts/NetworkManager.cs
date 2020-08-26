48437a4b5e8dcc5a07c1c4b1d60e0f24be1a3b40
Chris felts <echristianfelts@gmail.com>
2020-08-26 03:44:51 -0700

    First commit

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    //  instance
+    public static NetworkManager instace;
+
+    private void Awake()
+    {
+        instace = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    private void Start()
+    {
+        //  connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server");
+    }
+}
